e870cf217bfb0e27f6581bda3b7d36d81a2678ad
atta <atta@example.com>
2024-04-06 00:18:46 +0500

    commited

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,21 @@
-import 'package:flutter/material.dart' show AppBar, Center, Colors, MaterialApp, Scaffold, Text, runApp;
+import 'dart:ui';
+
+import 'package:flutter/material.dart'
+    show AppBar, Center, Colors, MaterialApp, Scaffold, Text, runApp;
 
 void main() {
   runApp(MaterialApp(
       home: Scaffold(
     backgroundColor: Colors.amber[400],
     appBar: AppBar(
+      backgroundColor: const Color.fromARGB(255, 29, 129, 179),
       title: const Center(
+        
         child: Text('Hello'),
       ),
     ),
-    body: ,
+    body: const Center(
+      child: Text('Pakistan'),
+    ),
   )));
 }
